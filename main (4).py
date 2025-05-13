dc41a2293151aefd9e0f85c61ab0fee163e6ea10
Prince Kumar <siharamkumar@gmail.com>
2025-05-13 20:16:13 +0530

    Add files via upload

---
+++ main (4).py
@@ -0,0 +1,12 @@
+n = int(input("Enter a number: "))
+even_sum = 0
+odd_sum = 0
+for i in range(1, n + 1):
+    if i % 2 == 0:
+        even_sum += i
+    else:
+        odd_sum += i
+print(f"Sum of even numbers = {even_sum}")
+print(f"Sum of odd numbers = {odd_sum}")
+if even_sum > odd_sum:
+    print
